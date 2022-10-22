10e3a9bbdbb9f1573198e4b6f3af650fe5096116
Edwin J. Medina <edwin.jmg@gmail.com>
2022-10-22 11:33:43 -0500

    Initial commit

---
+++ app/src/main/java/com/edwinjmg/appgrupo1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.edwinjmg.appgrupo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
